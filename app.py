be45175aa9d461bf210bc8c4fea2e849f5e81cb1
Sankeerth Kankati <127194463+sri-manojkumar@users.noreply.github.com>
2024-10-28 19:11:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
-from flask import Flask, render_template, request, jsonify
-import cv2
-import base64
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-
-@app.route('/capture', methods=['POST'])
-def capture():
-    # Initialize camera and capture image
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    cap.release()
-    
-    # If image is captured, convert to base64 for HTML display
-    if ret:
-        _, buffer = cv2.imencode('.jpg', frame)
-        img_str = base64.b64encode(buffer).decode('utf-8')
-        return jsonify({'status': 'success', 'image': img_str})
-    else:
-        return jsonify({'status': 'failed', 'image': None})
 
 if __name__ == '__main__':
     app.run(debug=True)
